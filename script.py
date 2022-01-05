77908229863429af3716d4affba3a1ecbc5b0ad3
RodrigoEC <rodrigo.cavalcanti@ccc.ufcg.edu.br>
2022-01-05 19:56:39 -0300

    bug fix

--- script.py
+++ script.py
@@ -7,7 +7,7 @@ def main():
     output_path=sys.argv[2]
 
     title = 'HTML file'
-    if (sys.argv[3]): title = sys.argv[3]
+    if (len(sys.argv) < 3): title = sys.argv[3]
 
     HTML_HEAD = f'''
 

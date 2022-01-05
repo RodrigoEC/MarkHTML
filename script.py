4f72724178ccd3d9c0aeb42ed6c5ac913cdbe8c7
RodrigoEC <rodrigo.cavalcanti@ccc.ufcg.edu.br>
2022-01-05 19:43:09 -0300

    Refactor of the main function to add head to html

--- script.py
+++ script.py
@@ -1,19 +1,42 @@
 import sys
-from markdown import markdownFromFile
+from markdown import markdown
+
+HTML_HEAD = '''
+
+<head>
+    <meta charset="utf-8">
+    <title>File</title>
+</head>
+
+'''
+
 
 def main():
     file_path=sys.argv[1]
     output_path=sys.argv[2]
-    print(file_path)
-    print(output_path)
 
+    file = open(file_path, 'r')
+    
+    text = file.read()
 
-    markdown_string = markdownFromFile(
-        input=file_path,
-        output=output_path,
-        encoding='utf8',
+    markdown_string = markdown(
+        text,
         extensions=['codehilite', 'extra', 'fenced_code']
     )
+
+
+    html_final_string = f'''
+<html>
+        {HTML_HEAD}
+<body>
+        {markdown_string}
+</body>
+</html>
+    '''
+    
+    output_file = open(output_path, 'w')
+    
+    output_file.write(html_final_string)
 
 
 

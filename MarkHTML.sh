70803fdc29cc711d57b67f1575a4dfca3db32603
RodrigoEC <rodrigo.cavalcanti@ccc.ufcg.edu.br>
2022-01-05 22:04:53 -0300

    Add cp style.cdd

--- MarkHTML.sh
+++ MarkHTML.sh
@@ -8,18 +8,23 @@ MarkHTML() {
 						printf("/%s", $x)
 						
 			}}')
-		echo $base_path
+		echo $2
 		if [ -z $2 ]
 		then
 			local output_path="$base_path/output.html"
+			cp "$(pwd)/style.css" $base_path
 
 		else
 			local output_path=$2
+			local css_path=$(echo $output_path | awk -F/ '{
+			for (x=2; x<=NF-1; x++) {
+						printf("/%s", $x)
+						
+			}}')
+			cp "$(pwd)/style.css" $css_path
 		fi
 
-		echo "Saída: $output_path"
 		exec python3 script.py $1 $output_path $3
-		cp "$(pwd)/style.css" $base_path/templates
 		
 
 	else

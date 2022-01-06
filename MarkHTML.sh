042a79b329aa8fa5118a0aa8e6500419af74e518
RodrigoEC <rodrigo.cavalcanti@ccc.ufcg.edu.br>
2022-01-06 10:58:42 -0300

    Refactor of css_path

--- MarkHTML.sh
+++ MarkHTML.sh
@@ -22,24 +22,20 @@ MarkHTML() {
 			
 		if [ -z $2 ]
 		then 
-			local base_path=$(echo $1 | awk -F/ '{ for (x=1; x<=NF-1; x++) { printf("%s/", $x) } }')
-			local css_path="${base_path}style.css"
+			local base_output_path=$(echo $1 | awk -F/ '{ for (x=1; x<=NF-1; x++) { printf("%s/", $x) } }')
+				local css_path="./style.css"
 		else
 			local base_output_path=$(echo $2 | awk -F/ '{ for (x=1; x<=NF-1; x++) { printf("%s/", $x) } }')
 			local output_path=$2
 
-			if [ -z $4 ]
-			then 
-				local css_path="${base_output_path}style.css"
-			else
+			if [ ! -z $4 ]
+			then
 				local css_path=$4
 			fi
 
 		fi
 
-		cp "$(pwd)/style.css" $css_path
- 
- 		exec python3 script.py $1 $output_path $3
+ 		exec python3 script.py $1 $output_path $3 $4
 		
 
 	else
@@ -49,4 +45,4 @@ MarkHTML() {
 	
 }
 
-MarkHTML $1 $2 $3 $4
+MarkHTML $1 $2 $3 $4 $5

dd869eac4ba5fda06f580eaaf5d4c3f8797ccd33
RodrigoEC <rodrigo.cavalcanti@ccc.ufcg.edu.br>
2022-01-05 21:27:36 -0300

    Add base style to the html generated

--- script.py
+++ script.py
@@ -1,20 +1,23 @@
 import sys
 from markdown import markdown
-
+from markdown.extensions.toc import TocExtension
 
 def main():
     file_path=sys.argv[1]
     output_path=sys.argv[2]
 
     title = 'HTML file'
-    print(len(sys.argv))
+    css_path = './style.css'
+
     if (len(sys.argv) > 3): title = sys.argv[3]
+    if (len(sys.argv) > 4): css_path = sys.argv[4]
 
     HTML_HEAD = f'''
 
     <head>
         <meta charset="utf-8">
         <title>{title}</title>
+        <link rel="stylesheet" type="text/css" href={css_path}>
     </head>
 
     '''
@@ -26,7 +29,9 @@ def main():
 
     markdown_string = markdown(
         text,
-        extensions=['codehilite', 'extra', 'fenced_code']
+        output_format='html5',
+        extensions=['codehilite', 'extra', 'fenced_code', TocExtension(baselevel=1)]
+
     )
 
 

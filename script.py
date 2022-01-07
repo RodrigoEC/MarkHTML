f8136c4a96fccc7382dc621379f1da6ec4da73c8
RodrigoEC <rodrigo.cavalcanti@ccc.ufcg.edu.br>
2022-01-06 23:03:36 -0300

    Refactor of the python script to add a identation prettify

--- script.py
+++ script.py
@@ -1,6 +1,34 @@
 import sys
 from markdown import markdown
 from markdown.extensions.toc import TocExtension
+from jinja2 import FileSystemLoader, Environment
+from bs4 import BeautifulSoup
+
+def parse_markdown(file_path):
+    file = open(file_path, 'r')
+    
+    text = file.read()
+
+    return markdown(
+        text,
+        output_format='html5',
+        extensions=['codehilite', 'extra', 'fenced_code', TocExtension(baselevel=1)]
+    )
+
+def create_html(title, css_path, html):
+    loader = FileSystemLoader('.')
+    env = Environment(loader=loader)
+    template = env.get_template('base.html')
+
+    parsed_html = template.render(
+        title=title,
+        css_path=css_path,
+        parsed_html=html
+    )
+
+    soup = BeautifulSoup(parsed_html, features="html.parser")
+
+    return soup.prettify()
 
 def main():
     file_path=sys.argv[1]
@@ -8,46 +36,17 @@ def main():
 
     title = 'HTML file'
     css_path = './style.css'
-    print(len(sys.argv))
 
     if (len(sys.argv) > 3): title = sys.argv[3]
     if (len(sys.argv) > 4): css_path = sys.argv[4]
 
-    HTML_HEAD = f'''
-
-    <head>
-        <meta charset="utf-8">
-        <title>{ title}</title>
-        <link rel="stylesheet" type="text/css" href={css_path}>
-    </head>
-
-    '''
-
-
-    file = open(file_path, 'r')
+    parsed_html = parse_markdown(file_path)
     
-    text = file.read()
+    final_html = create_html(title, css_path, parsed_html)
 
-    markdown_string = markdown(
-        text,
-        output_format='html5',
-        extensions=['codehilite', 'extra', 'fenced_code', TocExtension(baselevel=1)]
-
-    )
-
-
-    html_final_string = f'''
-<html>
-        {HTML_HEAD}
-    <body>
-        {markdown_string}
-    </body>
-</html>
-    '''
-    
     output_file = open(output_path, 'w')
-    
-    output_file.write(html_final_string)
+    output_file.write(final_html)
+    output_file.close()
 
 
 if __name__ == '__main__':
